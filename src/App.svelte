3f5454227b135c4005d4149c8cb7fa0879000885
eduoz90 <eduardoduenas961@gmail.com>
2023-01-07 17:03:44 -0500

    Segunda api en apiNasa

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+ 
+
+let key = "pVtPXBCbcd5uoafUYCibCAUDPbCuFqCGbjx0ZSwh";
+import Guardar from './lib/Nasa.svelte';
+let guardarurl = [];
+
+async function loadNasa(){
+   const respuesta = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=40&api_key=${key}`);
+   const data = await respuesta.json()
+   guardarurl = data.photos;
+}
+
+loadNasa()
+
+</script>
+
+<h1>app Nasa_api</h1>
+
+
+<div class="container">
+ <div class="grid">
+   {#each guardarurl as guardar}
+   <Guardar guardar={guardar}/>
+   {/each}
+ </div>
+</div>
+
+
+
+
---
+++ src/lib/Nasa.svelte
@@ -0,0 +1,11 @@
+<script>
+export  let guardar;
+</script>
+
+    
+
+  <div class="imagenes">
+    <img src={guardar.img_src} alt="imagenes_nasa">
+  </div>
+ 
+
